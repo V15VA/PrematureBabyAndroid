6b54e82974ae3e20ac3ca1989aface6fd9acc804
GH013 <ggh18@ic.ac.uk>
2021-01-11 13:47:00 +0000

    Example GraphView added to View Summary (with dependencies, XML and code on OnCreate class of activity). Example moving average filter class added.

--- app/src/main/java/com/example/prematurebabyandroid/SummaryActivity.java
+++ app/src/main/java/com/example/prematurebabyandroid/SummaryActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class SummaryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_summary);
+
+        //https://github.com/jjoe64/GraphView
+        //example code so demonstrate use
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
     }
 }
