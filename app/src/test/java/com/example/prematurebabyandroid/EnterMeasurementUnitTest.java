449908b4099594f0044ab191b52a579194df02d8
GH013 <ggh18@ic.ac.uk>
2021-01-14 18:10:34 +0000

    Unit Test for Enter Measurement with Mokito

---
+++ app/src/test/java/com/example/prematurebabyandroid/EnterMeasurementUnitTest.java
@@ -0,0 +1,37 @@
+package com.example.prematurebabyandroid;
+
+
+import android.content.Context;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EnterMeasurementUnitTest {
+
+    private static final String FAKE_STRING = "Entry was successful";
+
+    @Mock
+    Context mMockContext;
+
+    @Test
+    public void readStringFromContext_LocalizedString() {
+
+        NewMeasurementActivity myObjectUnderTest = new NewMeasurementActivity(mMockContext);
+
+        // ...when the string is returned from the object under test...
+
+
+        String result = myObjectUnderTest.validate("event","notes", "00:00:00",
+                0, 0, 0, 0);
+
+        // ...then the result should be the expected one.
+        assertThat(result, is(FAKE_STRING));
+    }
+
+}
